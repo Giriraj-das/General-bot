d8ce2084e77546aa604b685ad60a7db33aee2655
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-03-26 11:23:39 +0530

    del general parse mode

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from config import settings
-from app.handlers import router
+from app import router as main_router
 
 
 async def main():
-    bot = Bot(
-        token=settings.token,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-    )
+    bot = Bot(token=settings.token)
     dp = Dispatcher()
-    dp.include_router(router)
+    dp.include_router(main_router)
     await dp.start_polling(bot)
 
 
