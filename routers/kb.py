4b1fd0fdefb2321eae8d01240dbc9a51888b46db
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-03-29 16:33:51 +0530

    add InlineKeyboardMarkup for show cities

--- routers/kb.py
+++ routers/kb.py
@@ -1,4 +1,13 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
+
+from models import Location
+
+add_city = 'add_city'
 
 
 def start_keyboard() -> ReplyKeyboardMarkup:
@@ -20,3 +29,15 @@ def milk_keyboard() -> ReplyKeyboardMarkup:
         resize_keyboard=True,
         input_field_placeholder='Select item...'
     )
+
+
+def cities_keyboard(locations: list[Location]) -> InlineKeyboardMarkup:
+    rows = []
+    for location in locations:
+        rows.append([InlineKeyboardButton(
+            text=location.city,
+            callback_data=f'{location.city}-{location.latitude}-{location.longitude}',
+        )])
+
+    rows.append([InlineKeyboardButton(text=f'➕ Add a city', callback_data=add_city)])
+    return InlineKeyboardMarkup(inline_keyboard=rows)
