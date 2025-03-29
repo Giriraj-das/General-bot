64ae3d934e799e3772ef880d2fed53e00754337f
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-03-29 16:26:06 +0530

    create m2m relation

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     'db_helper',
     'Location',
     'User',
+    'UserLocation',
 )
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .location import Location
 from .user import User
+from .user_location import UserLocation
