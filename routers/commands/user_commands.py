6a8992fa34f9afa05dd7e82052ae06d8a90440ec
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-03-29 16:36:16 +0530

    add func shown cities list

--- routers/commands/user_commands.py
+++ routers/commands/user_commands.py
@@ -1,6 +1,8 @@
 from aiogram import Router, types, F
 
-from routers.kb import milk_keyboard
+from db_connections.weather import get_locations_by_user_tg_id
+from models import db_helper, Location
+from routers.kb import milk_keyboard, cities_keyboard
 
 router = Router(name=__name__)
 
@@ -14,7 +16,15 @@ async def handle_help(message: types.Message):
 
 
 @router.message(F.text.lower() == 'weather')
-async def handle_help(message: types.Message):
+async def show_cities_list(message: types.Message):
+    cities: list[Location] = []
+    async for session in db_helper.session_dependency():
+        cities = await get_locations_by_user_tg_id(
+            session=session,
+            user_tg_id=message.from_user.id,
+        )
+
     await message.answer(
-        text='Select a city or add it (name and coordinates).',
+        text='Select a city or add it.',
+        reply_markup=cities_keyboard(cities),
     )
