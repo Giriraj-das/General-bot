b31ec3505de72b9be93c4a8839642bfcfdaf0b76
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-04-03 13:58:09 +0530

    create milk supply service, change logic in sold service

--- services/milk.py
+++ services/milk.py
@@ -1,29 +1,67 @@
 from datetime import datetime, date
 
-from db_connections.milk import create_supply
-from models import db_helper
+from db_connections.milk import get_supply, create_supply, update_supply, create_sale
+from models import db_helper, Supply
 
 
 async def create_milk_supply_service(
         supply: str,
 ):
-    parts = supply.split()
+    parts: list[str] = supply.split()
 
-    supply_data = {}
-    if len(parts) == 1:
-        supply_data = {'quantity': float(parts[0])}
-    elif len(parts) == 2:
+    create_data: dict[str, float | date] = {
+        'quantity': float(parts[0]),
+        'current_date': date.today(),
+    }
+    update_data: dict[str, float] = {
+        'quantity': float(parts[0]),
+    }
+
+    if len(parts) == 2:
         try:
-            entered_date: date = datetime.strptime(parts[0], '%d.%m.%Y').date()
-            supply_data = {
-                'current_date': entered_date,
-                'quantity': float(parts[1]),
-            }
+            entered_date: date = datetime.strptime(parts[1], '%d.%m.%Y').date()
+            create_data['current_date'] = entered_date
         except ValueError:
             raise ValueError('Incorrect date')
 
     async for session in db_helper.session_dependency():
-        return await create_supply(
+        supply: Supply | None = await get_supply(
             session=session,
-            supply_data=supply_data,
+            current_date=create_data['current_date']
+        )
+        if not supply:
+            return await create_supply(
+                session=session,
+                supply_data=create_data,
+            )
+        else:
+            return await update_supply(
+                session=session,
+                supply=supply,
+                supply_data=update_data,
+            )
+
+
+async def create_milk_sold_service(
+        sale: str,
+):
+    parts: list[str] = sale.split('\n')
+
+    sale_data: dict[str, str | float | int | date] = {
+        'name': parts[0],
+        'quantity': float(parts[1]),
+        'price': int(parts[2]),
+        'current_date': date.today(),
+    }
+    if len(parts) == 4:
+        try:
+            entered_date: date = datetime.strptime(parts[3], '%d.%m.%Y').date()
+            sale_data['current_date'] = entered_date
+        except ValueError:
+            raise ValueError('Incorrect date')
+
+    async for session in db_helper.session_dependency():
+        return await create_sale(
+            session=session,
+            sale_data=sale_data,
         )
