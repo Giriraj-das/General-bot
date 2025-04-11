f20e380341376a4d35c545a60d41afdbb32e86e9
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-04-11 19:02:33 +0530

    change to bin/bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Applying migrations..."
 python -m alembic upgrade head
-
-if [ $? -ne 0 ]; then
-    echo "Migration failed!"
-    exit 1
-fi
 
 echo "Starting application..."
 exec python main.py
