1409e1e61e0d25e54043d11b4582ea88882ba939
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-04-11 17:09:29 +0530

    Fix invalid escape sequence in Accept header

--- utils/get_weather.py
+++ utils/get_weather.py
@@ -5,7 +5,7 @@ from config import settings
 
 async def get_weather(latitude: str, longitude: str):
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;\q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
     }
     params = {
